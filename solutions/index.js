6b2eec3ffa74c52bcb31290ff40e1a8bb60e8ca7
JuanValeraDev <juanvaleradev@gmail.com>
2024-09-10 20:50:24 +0200

    In exercise n3

--- solutions/index.js
+++ solutions/index.js
@@ -1,20 +1,73 @@
+
+//1.  Arrelgar función
+/*
 import net from 'node:net'
 
-export const ping = (ip) => {
+export const ping = (ip, callback) => {
     const startTime = process.hrtime()
 
-    const client = net.connect({ port: 80, host: ip }, () => {
+    const client = net.connect({port: 80, host: ip}, () => {
         client.end()
-        return { time: process.hrtime(startTime), ip }
+        callback(null, {time: process.hrtime(startTime), ip})
     })
 
     client.on('error', (err) => {
-        throw err
         client.end()
+        callback(err)
     })
 }
 
-ping('midu.dev', (err, info) => {
+ping('google.com', (err, info) => {
     if (err) console.error(err)
-    console.log(info)
+    else console.log(info)
 })
+
+ */
+
+//2. Pasar a promesas
+/*
+export async function obtenerDatosPromise() {
+    return new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => {
+                resolve({data: 'datos importantes'})
+            }, 2000)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+console.log(await obtenerDatosPromise())
+
+ */
+
+
+//3 - Explica qué hace la funcion. Identifica y corrige los errores en el siguiente código. Si ves algo innecesario, elimínalo. Luego mejoralo para que siga funcionando con callback y luego haz lo que consideres para mejorar su legibilidad.
+//Respuesta: Lo que hace la función es leer un fichero de entrada, pasar su contenido a mayúsculas y escribirlo en un fichero de salida.
+
+import fs from 'node:fs'
+
+export function procesarArchivo() {
+  fs.readFile('input.txt', 'utf8', (error, contenido) => {
+    if (error) {
+      console.error('Error leyendo archivo:', error.message);
+      return false;
+    }
+
+    setTimeout(() => {
+      const textoProcesado = contenido.toUpperCase();
+
+      fs.writeFile('output.txt', textoProcesado, error => {
+        if (error) {
+          console.error('Error guardando archivo:', error.message);
+          return false;
+        }
+
+        console.log('Archivo procesado y guardado con éxito');
+        return true
+      });
+
+    }, 1000);
+  });
+}
